d7bbf972ce0bb08367954bdd8caf6f158cbcae87
Leonard Asp <Leoasp96@gmail.com>
2017-02-14 10:57:27 +0100

    Added room and user data objects

--- quizmaniacs/src/main/java/se/quizmaniacs/Jdo/Room.java
+++ quizmaniacs/src/main/java/se/quizmaniacs/Jdo/Room.java
@@ -1,17 +1,52 @@
 package se.quizmaniacs.Jdo;
 
+import java.util.List;
+
 /**
  * Created by LeoAsp on 2017-02-13.
  */
 
 public class Room {
-    public String roomName;
-    public int playersConected;
-    public int maxPlayers;
+    String name;
+    int connectedPlayers;
+    int maxPlayers;
+    List<String> categories;
 
-    public Room(String roomName, int playersConected, int maxPlayers) {
-        this.roomName = roomName;
-        this.playersConected = playersConected;
+    public Room(String name, int maxPlayers) {
+        this.name = name;
+        this.connectedPlayers = 0;
         this.maxPlayers = maxPlayers;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getConnectedPlayers() {
+        return connectedPlayers;
+    }
+
+    public void setConnectedPlayers(int connectedPlayers) {
+        this.connectedPlayers = connectedPlayers;
+    }
+
+    public int getMaxPlayers() {
+        return maxPlayers;
+    }
+
+    public void setMaxPlayers(int maxPlayers) {
+        this.maxPlayers = maxPlayers;
+    }
+
+    public List<String> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<String> categories) {
+        this.categories = categories;
     }
 }
--- quizmaniacs/src/main/java/se/quizmaniacs/Jdo/User.java
+++ quizmaniacs/src/main/java/se/quizmaniacs/Jdo/User.java
@@ -5,7 +5,11 @@ package se.quizmaniacs.Jdo;
  */
 
 public class User {
-    String nickname;
+    private String nickname;
+
+    public User(String nickname) {
+        this.nickname = nickname;
+    }
 
     public String getNickname() {
         return nickname;
