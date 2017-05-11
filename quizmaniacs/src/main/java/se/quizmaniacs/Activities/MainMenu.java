33fc43e58e62d0d542f736c3aed22b18ddc1576b
Leonard Asp <Leoasp96@gmail.com>
2017-05-11 21:17:27 +0200

    removed comments from necesary code

--- quizmaniacs/src/main/java/se/quizmaniacs/Activities/MainMenu.java
+++ quizmaniacs/src/main/java/se/quizmaniacs/Activities/MainMenu.java
@@ -21,30 +21,29 @@ public class MainMenu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-       // new Thread(Controller.getConnectionHandler()).start();
+        new Thread(Controller.getConnectionHandler()).start();
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
         mainMenuPlayBtn = (Button) findViewById(R.id.mainMenuPlayBtn);
         mainMenuNickField = (EditText) findViewById(R.id.mainMenuNickField);
-
         mainMenuPlayBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 String nick = mainMenuNickField.getText().toString();
 
-                //try {
-                  //  Controller.getDataHandler().startThreads(Controller.getConnectionHandler().getSocket());
-               // } catch (IOException e) {
-                 //   e.printStackTrace();
-                //}
+                try {
+                    Controller.getDataHandler().startThreads(Controller.getConnectionHandler().getSocket());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
-               // Controller.getDataHandler().send(Controller.getCommandMaker().makeSetNickCommand(nick));
-                //Controller.getDataHandler().send(Controller.getCommandMaker().makeGetLobbyList());
-                startActivity(new Intent(MainMenu.this, ChatRoom.class));
-                
+                Controller.getDataHandler().send(Controller.getCommandMaker().makeSetNickCommand(nick));
+                Controller.getDataHandler().send(Controller.getCommandMaker().makeGetLobbyList());
+                startActivity(new Intent(MainMenu.this, LobbyMenu.class));
+
             }
         });
 
